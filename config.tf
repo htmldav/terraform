16f65bab186941f9e652fd4f7bcf3b98ec09553e
Alex <htmldav@mail.ru>
2022-03-23 05:20:13 +0300

    haus

--- config.tf
+++ config.tf
@@ -29,24 +29,13 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = "e2l4q2u32och1eqtqs2h"
     nat       = true
   }
- 
+
   metadata = {
-    ssh-keys = "ssh-rsa ubuntu:AAAAB3NzaC1yc2EAAAADAQABAAABAQCXgG/TOpdnA3pJTyi4UWv7xfLD7DrRWSkkNPi3W5fAQa0tDpsinG9l9fJCdgKPlsgl6wkHm6gFRu21kY9NZ5OzEB9rwPzQ6eC9shblL+eLgWdHhpKZPb6SuYJBg30ka38m9CD84LWPbQkuR2mvzrgeE/21OwoLmBDKndI0U/8NwdxWXv7BWL04kmhgsx0BaZRXZwf0gB3Qg0g1YKUG6Fr5JGtgnR3CvHXe1DT2Aent8E2RpNfFNaAm9NYiYLNnbq85t5iJKH6gqddMGiuQwlQaI6PIMQpJ02t1yTKiqTnL1IQJNYQbL85bHk13HLtAwMglLszBadE+4eV2ZezaZuvb rsa-key-20220121"
+    ssh-keys = "ubuntu:${file("~/.ssh/terraform_id_rsa.pub")}"
   }
-}
-
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 output "internal_ip_address_vm_1" {
@@ -56,4 +45,3 @@ output "internal_ip_address_vm_1" {
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
-
