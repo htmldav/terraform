22c3210066b2f83cb8ca747bb36c8ee09e7b2bdc
Alex <htmldav@mail.ru>
2022-04-08 18:58:55 +0300

    credentials('YC_ACCOUNT_KEY_FILE')

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -1,5 +1,12 @@
 pipeline {
     agent any
+    environment {
+        YC_ACCOUNT_KEY_FILE = credentials('YC_ACCOUNT_KEY_FILE')
+        YC_FOLDER_ID = credentials('YC_FOLDER_ID')
+        YC_SUBNET_ID = credentials('YC_SUBNET_ID')
+     
+    }
+
     tools {
         terraform 'terraform'
     }
@@ -27,34 +34,35 @@ pipeline {
             }
         }
         
-        stage('Create inventory file'){
+        // stage('Create inventory file'){
+        //     steps{
+        //         sh "echo [build] >> ~/home.inv"
+        //         sh "echo ${dd_ip} >> ~/home.inv"
+        //     }
+        // }
+        
+        stage('ssh'){
             steps{
-                sh "echo [build] >> ~/home.inv"
-                sh "echo ${dd_ip} >> ~/home.inv"
+                sh "ssh -o StrictHostKeyChecking=no  ubuntu@${dd_ip} uname -a"
+                // sshagent(['secretPemJenkins']) {
+                //     sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa jenkins@${dd_ip} uname -a"
+                // }
             }
         }
         
-        stage('sshagent'){
-            steps{
-                sshagent(['secretPemJenkins']) {
-                    sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa jenkins@${dd_ip} uname -a"
-                }
-            }
-        }
-        
-        stage('Git checkout playbook'){
-            steps{
-              git branch: 'main', url: 'https://github.com/htmldav/ansibleDiplom.git'
-            }
-        }
+        // stage('Git checkout playbook'){
+        //     steps{
+        //       git branch: 'main', url: 'https://github.com/htmldav/ansibleDiplom.git'
+        //     }
+        // }
         
         
 
-        stage('Delete inventory file'){
-            steps{
-                sh "rm -f ~/home.inv"
-                sh "rm -f ~/.ssh/known_hosts"
-            }
-        }
+        // stage('Delete inventory file'){
+        //     steps{
+        //         sh "rm -f ~/home.inv"
+        //         sh "rm -f ~/.ssh/known_hosts"
+        //     }
+        // }
     }
 }
