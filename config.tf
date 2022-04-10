c2145f7ca3c70a0cbb06b95df9ddfd9f266b0bd0
Alex <htmldav@mail.ru>
2022-04-10 18:15:27 +0300

    external_ip_address_vm

--- config.tf
+++ config.tf
@@ -50,10 +50,10 @@ resource "yandex_compute_instance" "vm" {
   }
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm.0.network_interface.0.nat_ip_address
+output "external_ip_address_vm" {
+  value = yandex_compute_instance.vm.*.network_interface.0.nat_ip_address
 }
 
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm.1.network_interface.0.nat_ip_address
-}
+# output "external_ip_address_vm_2" {
+#   value = yandex_compute_instance.vm.1.network_interface.0.nat_ip_address
+# }
